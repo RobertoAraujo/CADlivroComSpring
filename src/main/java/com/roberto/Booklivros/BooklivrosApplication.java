bcdac68b3dc1eff982a66af6ea8cb786b522da61
Roberto-Moraes <endriosrobert@gmail.com>
2021-05-12 09:55:37 -0500

    foi tudo agora

---
+++ src/main/java/com/roberto/Booklivros/BooklivrosApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.Booklivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklivrosApplication.class, args);
+	}
+
+}
